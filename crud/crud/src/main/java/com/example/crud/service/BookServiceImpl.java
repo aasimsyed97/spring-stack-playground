00cf754d9af3e33169d0bae1053dfa121f06deca
aasimsyed97 <sayedaasim555@gmail.com>
2025-06-24 00:01:34 +0530

    service updated with exceptions

--- crud/crud/src/main/java/com/example/crud/service/BookServiceImpl.java
+++ crud/crud/src/main/java/com/example/crud/service/BookServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.crud.dto.BookDto;
 import com.example.crud.exception.BookNotFoundException;
 import com.example.crud.model.Book;
 import com.example.crud.repository.BookRepository;
+import com.sun.source.tree.LambdaExpressionTree;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,14 +15,14 @@ import java.util.stream.Collectors;
 import static org.springframework.beans.BeanUtils.copyProperties;
 
 @Service
-public class BookServiceImpl implements BookService{
+public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
 
-    BookServiceImpl(BookRepository bookRepository){
+    BookServiceImpl(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
-     
+
 
     @Override
     public BookDto getBOokByTitle(String title) {
@@ -37,41 +38,49 @@ public class BookServiceImpl implements BookService{
 
     @Override
     public List<BookDto> getAllBooks() {
-         List<Book> bookList = bookRepository.findAll();
-
-        return bookList.stream().map(book-> {
-               BookDto dto = new BookDto();
-               copyProperties(book, dto);
-               return dto;
-           }).toList();
+        List<Book> bookList = bookRepository.findAll();
+        if (bookList.isEmpty()) {
+            throw new BookNotFoundException("No book is available in library");
+        }
+        return bookList.stream().map(book -> {
+            BookDto dto = new BookDto();
+            copyProperties(book, dto);
+            return dto;
+        }).toList();
     }
 
     @Override
     public BookDto saveBook(BookDto bookDto) {
-          Book book = new Book();
-          copyProperties(bookDto, book);
-           Book savedBook =  bookRepository.save(book);
+        Book book = new Book();
+        copyProperties(bookDto, book);
+        Book savedBook = bookRepository.save(book);
 
         return bookDto;
     }
 
     @Override
     public BookDto updateBook(BookDto bookDto) {
-        Optional<Book> book = bookRepository.findByTitle(bookDto.getTitle());
+        Optional<Book> bookOptional = bookRepository.findByTitle(bookDto.getTitle());
+        if (bookOptional.isEmpty()) {
+            throw new BookNotFoundException("Book not found with title " + bookDto.getTitle());
+        }
+        Book book = bookOptional.get();
         copyProperties(bookDto, book);
         System.out.println(book);
-         bookRepository.save(book);
-        return  bookDto;
+        bookRepository.save(book);
+        return bookDto;
     }
 
     @Override
     public String deleteBookByTitle(String title) {
-         Optional<Book> book = bookRepository.findByTitle(title);
-          if(book !=null){
-              bookRepository.delete(book);
-          }
+        Optional<Book> bookOptional = bookRepository.findByTitle(title);
+        if (bookOptional.isEmpty()) {
+            throw new BookNotFoundException("Book not found with title " + title);
+        }
+        Book book = bookOptional.get();
+        bookRepository.delete(book);
 
-      return "book by title "+ title + " deleted.";
+        return "book by title " + title + " deleted.";
     }
 
 
