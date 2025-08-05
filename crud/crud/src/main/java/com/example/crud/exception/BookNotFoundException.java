36391868846f81e88b53aab30432a7b1860da6fe
aasimsyed97 <sayedaasim555@gmail.com>
2025-08-06 01:23:32 +0530

    removed everything from folder

--- crud/crud/src/main/java/com/example/crud/exception/BookNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.crud.exception;
-
-public class BookNotFoundException extends RuntimeException{
-
-     public BookNotFoundException(){ }
-   public  BookNotFoundException(String message){
-         super(message);
-    }
-}
