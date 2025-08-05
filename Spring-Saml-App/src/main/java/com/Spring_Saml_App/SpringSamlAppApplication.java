4102957e1d18a870cfc07799ea3213be4e9fae91
aasimsyed97 <sayedaasim555@gmail.com>
2025-08-06 01:37:29 +0530

    new setup updated.

---
+++ Spring-Saml-App/src/main/java/com/Spring_Saml_App/SpringSamlAppApplication.java
@@ -0,0 +1,13 @@
+package com.Spring_Saml_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 	SpringSamlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSamlAppApplication.class, args);
+	}
+
+}
---
+++ Spring-Saml-App/src/test/java/com/Spring_Saml_App/SpringSamlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_Saml_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSamlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
