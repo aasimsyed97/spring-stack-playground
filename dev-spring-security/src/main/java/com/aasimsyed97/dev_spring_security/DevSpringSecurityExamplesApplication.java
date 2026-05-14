828d0c917bf3f306aef92ac7f5ddbe6da301e727
aasimsyed97 <sayedaasim555@gmail.com>
2026-05-15 01:18:22 +0530

    complete dev.security application

---
+++ dev-spring-security/src/main/java/com/aasimsyed97/dev_spring_security/DevSpringSecurityExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.aasimsyed97.dev_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSpringSecurityExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSpringSecurityExamplesApplication.class, args);
+	}
+
+}
---
+++ dev-spring-security/src/test/java/com/aasimsyed97/dev_spring_security/DevSpringSecurityExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aasimsyed97.dev_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSpringSecurityExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
